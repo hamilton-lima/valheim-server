d5dd9b2d18d699066076e616aa2210958f0bacf2
Hamilton Lima <hamilton.lima@gmail.com>
2021-03-10 19:35:08 -0500

    Store logs in a file

--- start_server.sh
+++ start_server.sh
@@ -2,12 +2,13 @@ export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
 export SteamAppId=892970
 
-echo "Starting Valheim server"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+echo "valheim.service: START ${TIMESTAMP}" >> /home/steam/valheim/server-log
 
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-/home/steam/valheim/valheim_server.x86_64  -nographics -batchmode -name "OincLand" -port 2456 -world "OincLand" -password "supersecret21" -public 0 > /dev/null &
+/home/steam/valheim/valheim_server.x86_64  -nographics -batchmode -name "OincLand" -port 2456 -world "OincLand" -password "supersecret21" -public 0 >> /home/steam/valheim/server-log &
 export LD_LIBRARY_PATH=$templdpath
 
 # Updated from https://gamesgds.com/valheim-how-to-set-up-dedicated-linux-server/
@@ -17,6 +18,6 @@ echo ""
 while :
 do
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
-echo "valheim.service: timestamp ${TIMESTAMP}"
+echo "valheim.service: ${TIMESTAMP}" >> /home/steam/valheim/server-log
 sleep 60
 done
