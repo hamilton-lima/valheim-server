84ce92bc255ad996620b1220e5cfaeee672bff06
Hamilton Lima <hamilton.lima@gmail.com>
2021-03-11 13:48:00 -0500

    Refactor folder structure to mount data folder

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,18 @@ FROM cm2network/steamcmd
 
 RUN echo "valheim custom 0.0.5"
 
+# Create folders
 RUN mkdir /home/steam/valheim
+
+# Install valheim server
 RUN /home/steam/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/steam/valheim +app_update 896660 validate +exit
 
-COPY start_server.sh /home/steam/valheim/
 COPY variables.env /home/steam/valheim/
+COPY start_server.sh /home/steam/valheim/
 
-COPY worlds /home/steam/.config/unity3d/IronGate/Valheim/worlds
-COPY default-config/* /home/steam/.config/unity3d/IronGate/Valheim
+RUN mkdir /home/steam/valheim-data
+VOLUME /home/steam/valheim-data
 
 CMD bash /home/steam/valheim/start_server.sh
-
 EXPOSE 2456/udp
 EXPOSE 2457/udp
--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+set -o xtrace
+
 export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
 export SteamAppId=892970
+
+# display version for log
+cat /etc/os-release
 
 # define environment variables based on local.env
 export $(cat /home/steam/valheim/variables.env | xargs)
@@ -13,7 +19,14 @@ echo "valheim.service: START $(date "+%Y-%m-%d %H:%M:%S")"
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-valheim_server.x86_64 -nographics -batchmode -name "$NAME" -port 2456 -world "$WORLD" -password "$PASSWORD" -public 0 &
+/home/steam/valheim/valheim_server.x86_64 -nographics -batchmode \
+-port 2456 \
+-public 0 \
+-name "$NAME" \
+-world "$WORLD" \
+-password "$PASSWORD" \
+-savedir "/home/steam/valheim-data" &
+
 export LD_LIBRARY_PATH=$templdpath
 echo "valheim.service: $(date "+%Y-%m-%d %H:%M:%S")" 
 
