98186958e723f11fdaf1046f8b9910497d468f98
Hamilton Lima <hamilton.lima@gmail.com>
2021-03-10 19:16:38 -0500

    Run server in background

--- start_server.sh
+++ start_server.sh
@@ -7,6 +7,16 @@ echo "Starting Valheim server"
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-/home/steam/valheim/valheim_server.x86_64  -nographics -batchmode -name "OincLand" -port 2456 -world "OincLand" -password "supersecret21" -public 0
-
+/home/steam/valheim/valheim_server.x86_64  -nographics -batchmode -name "OincLand" -port 2456 -world "OincLand" -password "supersecret21" -public 0 > /dev/null &
 export LD_LIBRARY_PATH=$templdpath
+
+# Updated from https://gamesgds.com/valheim-how-to-set-up-dedicated-linux-server/
+echo "Server started"
+echo ""
+
+while :
+do
+TIMESTAMP=$(date ‘+%Y-%m-%d %H:%M:%S’)
+echo “valheim.service: timestamp ${TIMESTAMP}”
+sleep 60
+done
